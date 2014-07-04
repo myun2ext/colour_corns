1c9307cb5b9e378b11b9ea032c56597e19360427
myun2ext <myun2@mbm.nifty.com>
2014-07-04 12:23:47 +0900

    Update structure.rb

--- lib/coloured_corn/date/structure.rb
+++ lib/coloured_corn/date/structure.rb
@@ -1,6 +1,15 @@
 module ColouredCorn
   class Date
     class Structure
+      @value = nil
+
+      def initialize(date)
+        if date.is_a? Date
+          @value = Date.parse(date)
+        else
+          @value = date
+        end
+      end
     end
   end
 end
